a24d786441ac36e6b925005dc1bfe0e1fb0750fb
Steve <11562741+stevotron@users.noreply.github.com>
2020-11-15 14:46:55 +0000

    blacklist updater done

--- BlacklistUpdater.php
+++ BlacklistUpdater.php
@@ -2,5 +2,35 @@
 
 class BlacklistUpdater
 {
+  public static function go()
+  {
+    new self();
+  }
 
+  public function __construct()
+  {
+    $options = getopt('ed');
+
+    if (isset($options['e'])) {
+      $this->update(true);
+      echo 'Enabled...';
+    } elseif (isset($options['d'])) {
+      $this->update(false);
+      echo 'Disabled...';
+    } else {
+      echo 'Please provide a valid argument \'-e\' (enable) or \'-d\' (disable)';
+    }
+
+    echo "\n";
+
+    exec("pihole restartdns reload");
+  }
+
+  private function update(bool $enable)
+  {
+    $bool = $enable ? '1' : '0';
+    $command = 'sqlite3 "/etc/pihole/gravity.db" "UPDATE domainlist SET enabled = ' . $bool . ' WHERE comment LIKE \'%[auto-on-off]%\';"';
+    
+    exec($command);
+  }
 }
