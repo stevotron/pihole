a29383cdadea3d47035eafb231c281b9469807ad
Steve <11562741+stevotron@users.noreply.github.com>
2020-12-20 19:04:42 +0000

    Allow group toggling

--- BlacklistUpdater.php
+++ BlacklistUpdater.php
@@ -7,30 +7,41 @@ class BlacklistUpdater
     new self();
   }
 
-  public function __construct()
+  private function __construct()
   {
     $options = getopt('ed');
 
     if (isset($options['e'])) {
-      $this->update(true);
-      echo 'Enabled...';
+      $this->update('Day time only', '[day-time-only]', true);
+      echo 'Domain blocking enabled...';
     } elseif (isset($options['d'])) {
-      $this->update(false);
-      echo 'Disabled...';
+      $this->update('Day time only', '[day-time-only]', false);
+      echo 'Domain blocking disabled...';
     } else {
       echo 'Please provide a valid argument \'-e\' (enable) or \'-d\' (disable)';
     }
 
     echo "\n";
+    echo 'Restarting Pi-hole DNS and reloading lists...';
+    echo "\n";
 
-    exec("pihole restartdns reload");
+    exec('pihole restartdns reload-lists');
   }
 
-  private function update(bool $enable)
+  private function update(?string $groupName, ?string $commentString, bool $enable)
   {
     $bool = $enable ? '1' : '0';
-    $command = 'sqlite3 "/etc/pihole/gravity.db" "UPDATE domainlist SET enabled = ' . $bool . ' WHERE comment LIKE \'%[auto-on-off]%\';"';
-    
-    exec($command);
+
+    if (isset($groupName)) {
+      $escapedGroupName = SQLite3::escapeString($groupName);
+      $command = 'sqlite3 "/etc/pihole/gravity.db" "UPDATE \'group\' SET enabled = ' . $bool . ' WHERE name = \\"' . $escapedGroupName . '\\";"';
+      exec($command);
+    }
+
+    if (isset($commentString)) {
+      $escapedCommentString = SQLite3::escapeString($commentString);
+      $command = 'sqlite3 "/etc/pihole/gravity.db" "UPDATE domainlist SET enabled = ' . $bool . ' WHERE comment LIKE \\"%' . $escapedCommentString . '%\\";"';
+      exec($command);
+    }
   }
 }
